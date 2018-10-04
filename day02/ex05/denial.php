c034f23bf83aed8d6d980bb7de8ea2c3d5d2cc23
Dylan Slogrove <dslogrove@gmail.com>
2018-10-04 15:13:07 +0200

    Cleaned up denial

--- day02/ex05/denial.php
+++ day02/ex05/denial.php
@@ -1,34 +1,20 @@
 #!/usr/bin/env php
 <?php
-function catch_parse($code, $message, $file, $line) {
-
-}
-
-function dont_shutdown()
-{
-	$last_error = error_get_last();
-	if ($last_error['type'] === E_PARSE)
-		catch_parse(E_PARSE, $last_error['message'], $last_error['file'], $last_error['line']);
-}
-
 if ($argc != 3)
 	return;
 if (!(file_exists($argv[1])))
 	return;
 $file = fopen($argv[1], 'r');
-$tab = [];
+$headers = fgetcsv($file, 0,';');
 while ($line = fgetcsv($file, 0,';'))
 	$tab[] = $line;
 fclose($file);
-$headers = array_shift($tab);
 if (($index = array_search($argv[2], $headers)) === false)
 	return;
 for ($i = 0; $headers[$i]; $i++)
 	foreach ($tab as $e)
 		${$headers[$i]}[$e[$index]] = $e[$i];
 $stdin = fopen("php://stdin", 'r');
-set_error_handler('catch_parse');
-register_shutdown_function('dont_shutdown');
 echo "Enter your command: ";
 while ($command = fgets($stdin))
 {
@@ -40,4 +26,5 @@ while ($command = fgets($stdin))
 	echo "Enter your command: ";
 }
 echo PHP_EOL;
+fclose($stdin);
 ?>
