e00a738621d1b87cf17f1d52c9568ecb513f3f14
Dylan Slogrove <dslogrove@gmail.com>
2018-10-05 13:49:36 +0200

    stuff

---
+++ day04/ex00/index.php
@@ -0,0 +1,10 @@
+<?php
+	session_start();
+	if()
+
+?>
+<form action="index.php" method ="get">
+	Username: <input type="text" name="login">
+	Password: <input type="text" name="passwd">
+	<input type="submit" value="ok">
+</form>
