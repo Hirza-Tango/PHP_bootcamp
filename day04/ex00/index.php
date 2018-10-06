688fe57f494ca457929814097ba9cbb392a01f3f
Dylan Slogrove <dslogrove@gmail.com>
2018-10-06 11:04:24 +0200

    Some stuff

--- day04/ex00/index.php
+++ day04/ex00/index.php
@@ -1,10 +1,17 @@
 <?php
 	session_start();
-	if()
-
+	if ($_GET['submit'] === "OK")
+	{
+		if($_GET['login'])
+			$_SESSION['login'] = $_GET['login'];
+		if($_GET['passwd'])
+			$_SESSION['passwd'] = $_GET['passwd'];
+	}
 ?>
+<html><body>
 <form action="index.php" method ="get">
-	Username: <input type="text" name="login">
-	Password: <input type="text" name="passwd">
-	<input type="submit" value="ok">
+	Username: <input type="text" name="login" value="<?=$_SESSION['login']?>" /><br/>
+	Password: <input type="text" name="passwd" value="<?=$_SESSION['passwd']?>" /><br/>
+	<input type="submit" name="submit" value="OK"/>
 </form>
+</html></body>
---
+++ day04/ex01/create.php
@@ -0,0 +1,25 @@
+<?php
+	if ($_POST['submit'] !== "OK" || !$_POST['passwd'] || !$_POST['login'])
+	{
+		echo "ERROR\n";
+		return;
+	}
+	if (!file_exists("/private/passwd"))
+	{
+		@mkdir("/private");
+		$arr = [];
+	}
+	else
+		$arr = unserialize(file_get_contents("/private/passwd"));
+	foreach ($arr as $e)
+		if ($e['login'] === $_POST['login'])
+		{
+			echo "ERROR\n";
+			return;
+		}
+	$arr[] = Array('login'=>$_POST['login'], 'passwd'=>hash("sha512", $_POST['passwd']));
+	echo String(file_put_contents("/private/passwd", serialize($arr)));
+	print_r($arr);
+	echo "OK\n";
+	return;
+?>
