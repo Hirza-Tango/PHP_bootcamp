dee748b6ff5d8b0c28e76301090f2df76ee15fa9
Dylan Slogrove <dslogrove@gmail.com>
2018-10-11 12:03:38 +0200

    WH40K

---
+++ day08/ex00/Ship.class.php
@@ -0,0 +1,17 @@
+<?php
+
+class Ship{
+	public $name;
+	public $size;
+	public $sprite;
+	public $hull_points_max;
+	public $engine_power;
+	public $speed;
+	public $handling;
+	public $shield;
+	public $weapons;
+
+	public $hull_points_current;
+	public $shield_pp;
+	//TODO:
+}
---
+++ day08/ex00/Weapon.class.php
@@ -0,0 +1,12 @@
+<?php
+
+class Weapon{
+	public $charge;
+	public $range_short;
+	public $range_medium;
+	public $range_long;
+
+	public function shoot(){
+		
+	}
+}
